b255f61995d877e87dc439f22abbde0bb119bb31
Beatriz <beatrizleite_aragao@hotmail.com>
2023-02-26 02:33:32 -0300

    validação de cpf

--- js/form.js
+++ js/form.js
@@ -11,55 +11,63 @@ const rua = document.getElementById('rua');
 const mensagem = document.querySelector('#mensagem');
 const notNull = document.getElementsByClassName('not-null');
 
-function isEmpty(elem){
-    return elem.value.length < 1 ? `O campo <strong>${elem.name}</strong> não pode ser vazio.` : ''; 
+function isEmpty(elem) {
+    return elem.value.length < 1 ? `O campo <strong>${elem.name}</strong> não pode ser vazio.` : '';
 }
 
-function validaEmail(elem){
+function validaEmail(elem) {
     //emailregex
     //pode fazer tanto no match quanto test
     return elem.value.match(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/) ? '' : `Digite um <strong>e-mail</strong> válido`;
 }
 //para validar cep
-function validaCEP(elem){
-    if(!elem.value.match(/^[0-9]{8}$/)) 
+function validaCEP(elem) {
+    if (!elem.value.match(/^[0-9]{8}$/))
         return `Digite um <strong> CEP </strong> válido.`;
     else return '';
 }
 
+//para validar cpf
+function validaCPF(elem) {
+    return elem.value.match(/^[0-9]{11}$/) ? '' : `Digite um <strong>cpf</strong> válido`;
+}
+
 function updateAdress(data) {
-    if( !('erro' in data)) {
-        rua.value=(data.logradouro);
-        bairro.value=(data.bairro);
-        cidade.value=(data.localidade);
-        uf.value=(data.uf);
+    if (!('erro' in data)) {
+        rua.value = (data.logradouro);
+        bairro.value = (data.bairro);
+        cidade.value = (data.localidade);
+        uf.value = (data.uf);
         mensagem.innerHTML = '';
     } else {
         mensagem.innerHTML = `CEP não encontrado`;
     }
 }
 
-form.addEventListener('submit', function(event){
+form.addEventListener('submit', function (event) {
     event.preventDefault();
 
     let msg = [];
     let markup = '';
 
-   //pra pegar todos os elementos que tem essa classe
+    //pra pegar todos os elementos que tem essa classe
     Array.from(notNull).forEach(field => {
         let fieldState = isEmpty(field);
-        if(fieldState) 
+        if (fieldState)
             msg.push(fieldState);
     });
     //não é bom fazer muito uso de addEventListener = diminui a performance
 
     const isEmail = validaEmail(email);//validação do email
-    if(isEmail) msg.push(isEmail); //se houver retorno , dá um push
+    if (isEmail) msg.push(isEmail); //se houver retorno , dá um push
+
+    const isCPF = validaCPF(cpf);//validação de cpf
+    if(isCPF) msg.push(isCPF);//se houver retorno de invalidação dá um push
 
     const isCEP = validaCEP(cep);
-    if(isCEP.length > 0) {
+    if (isCEP.length > 0) {
         msg.push(isCEP);
-    } else {  
+    } else {
         //injeção de script
         const script = document.createElement('script');
         script.src = 'https://viacep.com.br/ws/' + cep.value + '/json?callback=updateAdress';
@@ -70,11 +78,11 @@ form.addEventListener('submit', function(event){
         markup += `<p>${item}</p>` //retorna uma linha de parágrafo
     });
 
-    
-    if(msg.length == 0){
+
+    if (msg.length == 0) {
         form.submit(); //faz a o envio do formulário
-    } 
-    else{
+    }
+    else {
         mensagem.innerHTML = markup;
     }
 
